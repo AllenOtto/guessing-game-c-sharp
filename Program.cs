f55768aa2890ffea325ee80f70bab506b4f64617
AllenOtto <kephamoturi@gmail.com>
2023-06-27 20:15:06 +0300

    Added comments and formatting

--- Program.cs
+++ Program.cs
@@ -1,25 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Generate random number to use as secret number
 Random randInt = new Random();
 int secretNumber = randInt.Next(1, 11);
+// Variable to hold user guess integer
 int userGuess = 0;
+// Control variables for while loop
 int guessCount = 0;
 int guessLimit = 3;
 
+// While user guess is wrong and tries are less than limit, run.
 while(userGuess != secretNumber && guessCount < guessLimit) {
     Console.Write("I am a number between 1 and 10. Guess which: ");
     try {
         userGuess = Convert.ToInt32(Console.ReadLine());
     } catch(Exception e) {
-        Console.Write(e.Message);
+        Console.Write(e.Message + "\n");
     }
-    Console.Write(secretNumber + "\n");
 
+    // 
     guessCount++;
 }
 
+// Check if user won or ran out of tries
 if(userGuess == secretNumber) {
-    Console.WriteLine("Very nice! You Win");
+    Console.WriteLine("Congrats! You Got It!");
 } else {
-    Console.WriteLine("Better luck nes'time, chomp");
+    Console.WriteLine($"Better luck nes'time, chomp. Secret number was {secretNumber}");
 }
